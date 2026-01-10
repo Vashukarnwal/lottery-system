fc9f8a752346ef2fcb3411bf02287bbaeef86af4
Vashu Karnwal <karnwalvashu@gmail.com>
2026-01-10 12:45:11 +0530

    fix result check server error

--- public/login.js
+++ public/login.js
@@ -4,15 +4,19 @@ function login() {
 
   fetch("/login", {
     method: "POST",
+    credentials: "include",   // ⭐⭐⭐ MOST IMPORTANT
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
   })
     .then(res => res.json())
     .then(data => {
       if (data.success) {
-        window.location.href = "/admin"; // 🔥 yahin se admin open
+        window.location.href = "/admin"; // 🔥 admin open
       } else {
         document.getElementById("msg").innerText = "Invalid login";
       }
+    })
+    .catch(err => {
+      document.getElementById("msg").innerText = "Server error";
     });
 }
